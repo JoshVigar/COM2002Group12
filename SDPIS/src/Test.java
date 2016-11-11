91d010322693fe258653b8606286e10b7a530206
babatundeadeola <badeola1@sheffield.ac.uk>
2016-11-11 14:01:12 +0000

    created database tables

--- SDPIS/src/Test.java
+++ SDPIS/src/Test.java
@@ -13,21 +13,20 @@ public class Test {
                 //String sql = "CREATE TABLE group_members(first_name VARCHAR(30), last_name VARCHAR(30), age INTEGER, PRIMARY KEY(last_name))" ;
                 //stmt.executeUpdate(sql);
 
-                String sql = "SELECT * FROM group_members";
-                ResultSet rs = stmt.executeQuery(sql);
-                //STEP 5: Extract data from result set
-                while(rs.next()){
-                    //Retrieve by column name
-                    String first = rs.getString("first_name");
-                    String last = rs.getString("last_name");
-                    int age = rs.getInt("age");
+                String sql = "CREATE TABLE subscription(SubscriptionID INTEGER NOT NULL, SubscriptionTitle VARCHAR,MonthlyCost INTEGER, CheckUp INTEGER, HygienistVisits INTEGER, Repairs INTEGER, PRIMARY KEY (SubscriptionID))";
+                stmt.executeUpdate(sql);
+                sql = "CREATE TABLE address(AddressID VARCHAR, HouseNum INTEGER, Street VARCHAR, City VARCHAR, Region VARCHAR, PostCode VARCHAR, PRIMARY KEY (AddressID))";
+                stmt.executeUpdate(sql);
+                sql = "CREATE TABLE customer(ID INTEGER NOT NULL, Title VARCHAR,FName VARCHAR(30), LName VARCHAR(30),BirthDate DATE, PhoneNum VARCHAR(15), PRIMARY KEY (ID))";
+                stmt.executeUpdate(sql);
+                sql = "CREATE TABLE visitType(TypeID INTEGER, Partner VARCHAR, Duration TIME, Cost INTEGER , PRIMARY KEY (TypeID))";
+                stmt.executeUpdate(sql);
+                sql = "CREATE TABLE appointment(TypeOfVisit INTEGER, StartTime TIMESTAMP, EndTime TIMESTAMP, PRIMARY KEY (TypeOfVisit, StartTime))";
+                stmt.executeUpdate(sql);
 
-                    //Display values
-                    System.out.print("First: " + first);
-                    System.out.print(" Last: " + last);
-                    System.out.println(" Age: " + age);
-                }
-                rs.close();
+
+
+                stmt.close();
             }
             catch (SQLException ex) {
                 ex.printStackTrace(); }
