15369a3d3a716e2ffe463f8fbb47a824efabfe67
Josh <joshxx2@btinternet.com>
2016-11-15 18:08:25 +0000

    Added ManagePatients

--- SDPIS/src/SecretaryGUI.java
+++ SDPIS/src/SecretaryGUI.java
@@ -37,9 +37,8 @@ public class SecretaryGUI extends JFrame{
         btnMP.addActionListener(
                 new ActionListener(){
                     public void actionPerformed(ActionEvent e){
-                        //View/edit patients outstanding balance
-                        //View treatment record
-                        //
+                        dispose();
+                        new SecretaryGUI().ManagePatients();
                     }
                 }
         );
@@ -132,6 +131,75 @@ public class SecretaryGUI extends JFrame{
                                 (String)aType.getSelectedItem()};
 
                         System.out.print(Arrays.toString(newBooking));
+
+                    }
+                }
+        );
+
+        JButton btnBack = new JButton("Go Back");
+        btnBack.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        dispose();
+                        new SecretaryGUI().SecretaryGUI();
+                    }
+                }
+        );
+
+        int bHeight = (int)(this.getHeight()*0.1);
+        int bWidth = (int)(this.getWidth()*0.1);
+
+        Container contentpane = getContentPane();
+        contentpane.add(title, BorderLayout.NORTH);
+        contentpane.add(inputsPanel, BorderLayout.CENTER);
+        contentpane.add(btnBack, BorderLayout.SOUTH);
+
+
+        inputsPanel.setBorder(BorderFactory.createEmptyBorder(bHeight,bWidth,bHeight,bWidth));
+
+        //Don't forget to pack!
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void ManagePatients(){
+
+        setTitle("Sheffield Dental Practice");
+        setSize(500,600);
+
+        JLabel title = new JLabel("Enter Patient Details");
+        JLabel pID = new JLabel("PatientID:");
+        final JTextField txtPID = new JTextField(20);
+        JLabel fName = new JLabel("Forename:");
+        final JTextField txtFName = new JTextField(20);
+        JLabel sName = new JLabel("Sirname:");
+        final JTextField txtSName = new JTextField(20);
+        String[] sub = {"Subscribe","Cancel Subscription"};
+        JLabel operation= new JLabel("Operation:");
+        final JComboBox Sub = new JComboBox(sub);
+        JButton bSubmit = new JButton("Submit");
+
+        JPanel inputsPanel = new JPanel();
+        inputsPanel.add(pID);
+        inputsPanel.add(txtPID);
+        inputsPanel.add(fName);
+        inputsPanel.add(txtFName);
+        inputsPanel.add(sName);
+        inputsPanel.add(txtSName);
+        inputsPanel.add(operation);
+        inputsPanel.add(Sub);
+        inputsPanel.setLayout(new BoxLayout(inputsPanel, BoxLayout.Y_AXIS));
+
+
+        bSubmit.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        String[] managedPatient = {txtPID.getText(), (String)Sub.getSelectedItem(), txtFName.getText(),
+                                txtSName.getText()};
+
+                        System.out.print(Arrays.toString(managedPatient));
 
                     }
                 }
