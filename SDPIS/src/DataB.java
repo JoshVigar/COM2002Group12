b6653c2f3ff8069500c4a80ee0b20013c801e388
babatundeadeola <badeola1@sheffield.ac.uk>
2016-11-17 13:05:41 +0000

    working on result sets

--- SDPIS/src/DataB.java
+++ SDPIS/src/DataB.java
@@ -34,29 +34,65 @@ public class DataB {
                 stmt.executeUpdate(sql);
 */
 
-/*
-                String sql = "SELECT * FROM Customer";
-                ResultSet rs = stmt.executeQuery(sql);
-                while(rs.next()){
-                    //Retrieve by column name
-                    int id = rs.getInt("ID");
-                    String title = rs.getString("Title");
-                    String first = rs.getString("Fname");
-                    String last = rs.getString("Lname");
-                    Date dob = rs.getDate("BirthDate");
-                    String phone = rs.getString("PhoneNum");
-                    String address = rs.getString("AddressID");
+            String sql = "SELECT * FROM Customer";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                //Retrieve by column name
+                int id = rs.getInt("ID");
+                String title = rs.getString("Title");
+                String first = rs.getString("Fname");
+                String last = rs.getString("Lname");
+                Date dob = rs.getDate("BirthDate");
+                String phone = rs.getString("PhoneNum");
+                String address = rs.getString("AddressID");
 
-                    //Display values
-                    System.out.print("ID: " + id);
-                    System.out.print(", Title: " + title);
-                    System.out.print(", First: " + first);
-                    System.out.print(", Last: " + last);
-                    System.out.print(", DOB: " + dob);
-                    System.out.print("Phone: " + phone);
-                    System.out.print("AddressID: " + address);
-                }
-*/
+                //Display values
+                System.out.print("ID: " + id);
+                System.out.print(", Title: " + title);
+                System.out.print(", First: " + first);
+                System.out.print(", Last: " + last);
+                System.out.print(", DOB: " + dob);
+                System.out.print("Phone: " + phone);
+                System.out.println("AddressID: " + address);
+            }
+
+            sql = "SELECT * FROM Address";
+            rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                String a =rs.getString("AddressID");
+                String b =rs.getString("HouseNum");
+                String c =rs.getString("Street");
+                String d =rs.getString("City");
+                String e =rs.getString("Region");
+                String f =rs.getString("PostCode");
+
+                System.out.print("AddressID:"+a);
+                System.out.print(" House No.:"+b);
+                System.out.print(" Street:"+c);
+                System.out.print(" City:"+d);
+                System.out.print(" Region:"+e);
+                System.out.println(" PostCode:"+f);
+            }
+
+            sql = "SELECT * FROM Subscription";
+            rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                int a =rs.getInt("SubscriptionID");
+                String b =rs.getString("SubscriptionTitle");
+                int c =rs.getInt("MonthlyCost");
+                int d =rs.getInt("CheckUp");
+                int e =rs.getInt("HygieneVisit");
+                int f =rs.getInt("Repair");
+                Date g =rs.getDate("EndDate");
+
+                System.out.print("SubscriptionID:"+a);
+                System.out.print(" SubscriptionTitle:"+b);
+                System.out.print(" MonthlyCost:"+c);
+                System.out.print(" CheckUp:"+d);
+                System.out.print(" HygieneVisit:"+e);
+                System.out.print(" Repair:"+f);
+                System.out.println(" EndDate:"+g);
+            }
 
             stmt.close();
 
